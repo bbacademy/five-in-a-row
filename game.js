6e37df19e0c25c914c365b712282592a9fef4fb1
Phong Vu <phong.vu@me.com>
2021-09-18 23:51:37 +0700

    update code

--- game.js
+++ game.js
@@ -1,57 +1,59 @@
-class Point {
+class Entry {
 	constructor(x, y) {
 		this.x = x
 		this.y = y
 	}
-	diff(x, y) {
-		return this.x !== x || this.y !== y
+	isEqual(x, y) {
+		return this.x === x && this.y === y
 	}
 	project(dx, dy, size = 5) {
 		const { x, y } = this
 		const line = []
 		for (let i = 0; i < size; i++)
-			line[i] = new Point(x + dx * i, y + dy * i)
+			line[i] = new Entry(x + dx * i, y + dy * i)
 		return line
 	}
 }
-class Points extends Array {
+class Entries extends Array {
 	constructor(id) {
 		super()
 		this.id = id
 	}
 }
 class Game {
-	
-	player = null // next player
-	player1 = new Points('A')
-	player2 = new Points('B')
-
+	ended = null
+	moves = [
+		new Entries('x'),
+		new Entries('o')
+	]
+	get player() {
+		return this.moves[0].id
+	}
 	reset() {
-		this.player1.length = 0
-		this.player2.length = 0
-		this.steps = 0
+		this.ended = null
+		this.moves.forEach(d => (d.length = 0))
 	}
 	check() {
-		for (let i of this.player) {
+		for (let i of this.moves[0]) {
 			const lines = [
 				i.project(1, 0), // horizontal
 				i.project(0, 1), // vertical
 				i.project(1, 1), // diagonal
 				i.project(-1, 1) // diagonal
 			]
-			const match = lines.find(line => {
+			this.ended = lines.find(line => {
 				return line.every(({ x: ax, y: ay }) => {
-					return this.player.some(({ x: bx, y: by }) => (ax == bx && ay == by))
+					return this.moves[0].some(({ x: bx, y: by }) => (ax == bx && ay == by))
 				})
 			})
-			if (match) return match
+			if (this.ended) break
 		}
-		return null
 	}
 	move(x, y) {
-		if (this.player1.every(i => i.diff(x, y)) && this.player2.every(i => i.diff(x, y))) {
-			this.player = this.player == this.player1 ? this.player2 : this.player1
-			this.player.push(new Point(x, y))
+		if (this.moves.every(a => a.every(b => !b.isEqual(x, y)))) {
+			this.moves.push(this.moves.shift())
+			this.moves[0].push(new Entry(x, y))
+			this.check()
 			return true
 		}
 		return false
